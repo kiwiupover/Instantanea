7b2f46058683af8062fd553e42ba343d1c286c13
Kiwiupover <kiwiupover@gmail.com>
2012-02-07 19:48:59 -0800

    added twitter signin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120208025225) do
+ActiveRecord::Schema.define(:version => 20120208033940) do
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "websites", :force => true do |t|
     t.string   "name"
