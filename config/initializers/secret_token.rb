063cb02d46c3ea0441510c7f2904fa5157d697d1
Kiwiupover <kiwiupover@gmail.com>
2012-02-07 18:49:13 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Instantanea::Application.config.secret_token = '71a7944031119aae86c785b62c56790585809e14f24bcf438a58231805c2f23fc77e2602b251474830d4791fb1292c1a592ff55e6c09f59f21a0da56fc3fd356'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Instantanea::Application.config.session_store :cookie_store, key: '_instantanea_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Instantanea::Application.config.session_store :active_record_store
