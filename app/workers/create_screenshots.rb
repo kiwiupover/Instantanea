0d0542f0436c0e99af270a645d41b965be14859b
Kiwiupover <kiwiupover@gmail.com>
2012-03-07 09:25:56 -0800

    added index.html

--- app/workers/create_screenshots.rb
+++ app/workers/create_screenshots.rb
@@ -6,8 +6,7 @@ class CreateScreenshots < Struct.new(:id)
     kit = IMGKit.new("#{page.url}") 
     path = "#{Rails.root}/public/#{page.title.downcase.gsub(" ","_")}.jpg"  
     file = kit.to_file(path)  
-    pi.screen_shot = File.open(path)
-    binding.pry 
+    pi.screen_shot = File.open(path) 
     pi.save!
   end
   
